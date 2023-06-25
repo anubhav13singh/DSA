4424017edb35f8d7061f859996bfe7c0fa318f6b
anubhav13singh <120702220+anubhav13singh@users.noreply.github.com>
2023-06-25 16:34:32 +0530

    Add files via upload

---
+++ Binary Search/ques1.js
@@ -0,0 +1,27 @@
+<aside>
+💡 **Question 1**
+
+****You are given an `m x n` integer matrix `matrix` with the following two properties:
+
+- Each row is sorted in non-decreasing order.
+- The first integer of each row is greater than the last integer of the previous row.
+
+Given an integer `target`, return `true` *if* `target` *is in* `matrix` *or* `false` *otherwise*.
+
+You must write a solution in `O(log(m * n))` time complexity.
+
+**Example 1:**
+
+!https://assets.leetcode.com/uploads/2020/10/05/mat.jpg
+
+```
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+```
+
+**Example 2:**
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+</aside>
