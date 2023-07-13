74e0985790aef078fa752f2aa47924d2525be6f7
anubhav13singh <120702220+anubhav13singh@users.noreply.github.com>
2023-07-13 10:36:59 +0530

    some ques left

---
+++ Recurssion/Assignment9/script7.js
@@ -0,0 +1,24 @@
+<aside>
+Given a string S, the task is to write a program to print all permutations of a given string.
+
+**Example 1:**
+
+***Input:***
+
+*S = “ABC”*
+
+***Output:***
+
+*“ABC”, “ACB”, “BAC”, “BCA”, “CBA”, “CAB”*
+
+**Example 2:**
+
+***Input:***
+
+*S = “XY”*
+
+***Output:***
+
+*“XY”, “YX”*
+</aside>
+      console.log(object);
